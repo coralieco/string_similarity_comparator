e37e710accdc31b28ae32b9e89f542d2fd7dc34b
Coralie Collignon <coralie.collignon@gmail.com>
2019-02-11 14:10:49 +0100

    refactor: use module StringSimilarityComparator

---
+++ spec/string_similarity_comparator_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe StringSimilarityComparator do
+  it 'has a version number' do
+    expect(StringSimilarityComparator::VERSION).not_to be nil
+  end
+end
